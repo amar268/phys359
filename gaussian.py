6a3cdc76ccb3733cbc63def00004dbe16d4b0b47
ClementFortin <68882482+ClementFortin@users.noreply.github.com>
2022-01-10 17:18:57 -0500

    Add gaussian.py file

---
+++ gaussian.py
@@ -0,0 +1,68 @@
+import spinmob as s
+import mcphysics
+import numpy as np
+
+def fit(peak_domain=None):
+    """ Function to fit Gaussian to compton scattering data. 
+    
+    Parameter:
+    ----------
+    peak_domain: list of size 2;
+    
+    Returns:
+    --------
+    fit_parameters: parameters obtained through fitting;  
+    """
+
+    data_set = mcphysics.data.load_chns() # load .chn data files
+    runs = len(data_set) # number of data files loaded
+    f = s.data.fitter() # initiate fitter object
+    
+    # initiate parameter arrays
+    A = np.zeros(runs) 
+    A_std = np.zeros(runs)
+    b = np.zeros(runs)
+    b_std = np.zeros(runs)
+    sigma = np.zeros(runs)
+    sigma_std = np.zeros(runs)
+
+    if peak_domain is not None:
+        if not isinstance(peak_domain, list) or len(peak_domain) != 2:
+            return TypeError('peak_domain must be a list of size 2.')
+
+    if runs==1: # single run
+        data = data_set[0] 
+        f.set_functions(f = 'A * exp(-0.5*((x - b)/sigma)**2)/(sigma*sqrt(2*pi))', p = 'A, b=1300, sigma = 10')
+        if peak_domain is None:
+            f.set_data(xdata = data['Channel'], ydata = data['Counts'], xlabel='Channel', ylabel='Counts')
+        else:
+            f.set_data(xdata = data['Channel'][peak_domain[0]:peak_domain[1]], ydata = data['Counts'][peak_domain[0]:peak_domain[1]], xlabel='Channel', ylabel='Counts')
+        f.fit() # fit to data
+        A = f.get_fit_results()['A']
+        A_std = f.get_fit_results()['A.std']
+        b = f.get_fit_results()['b']
+        b_std = f.get_fit_results()['b.std']
+        sigma = f.get_fit_results()['sigma']
+        sigma_std = f.get_fit_results()['sigma.std']
+
+    else: # multiple runs
+        for i in range(0,runs):
+            data = data_set[i]
+            f.set_functions(f = 'A * exp(-0.5*((x - b)/sigma)**2)/(sigma*sqrt(2*pi))', p = 'A, b=1300, sigma = 10')
+            if peak_domain is None:
+                f.set_data(xdata = data['Channel'], ydata = data['Counts'], xlabel='Channel', ylabel='Counts')
+            else:
+                print(data)
+                f.set_data(xdata = data['Channel'][peak_domain[0]:peak_domain[1]], ydata = data['Counts'][peak_domain[0]:peak_domain[1]], xlabel='Channel', ylabel='Counts')
+            f.fit()
+            A[i] = f.get_fit_results()['A']
+            f.fit() # fit to data
+            A_std[i] = f.get_fit_results()['A.std']
+            b[i] = f.get_fit_results()['b']
+            b_std[i] = f.get_fit_results()['b.std']
+            sigma[i] = f.get_fit_results()['sigma']
+            sigma_std[i] = f.get_fit_results()['sigma.std']
+    fit_parameters = [A, A_std, b, b_std, sigma, sigma_std]
+    return fit_parameters
+
+#__________________________________________________________________________________
